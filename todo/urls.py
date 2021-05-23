607a1bf81068b42bcb94259c7e008536474cf171
ShafiArnob <shafiarnob@gmail.com>
2021-05-23 21:14:04 +0600

    create app & run

--- todo/urls.py
+++ todo/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# Change
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+    # arnob 1234
     path('admin/', admin.site.urls),
+    path('',include('tasks.urls')),
 ]
